d1d6149b5d12d50616ee865756b3081f1ab04d1a
gleniosp <glenio.sp@gmail.com>
2020-03-29 17:08:51 -0300

    Changes for working on Windows

--- ui/src/service/bookService.js
+++ ui/src/service/bookService.js
@@ -6,29 +6,24 @@ if (process.env.BOOK_SERVICE_URL) {
   baseUrl = process.env.BOOK_SERVICE_URL;
 }
 
-export const SearchBooks = async (query, page, perPage, newEndpoint) => {
+export const SearchBooks = async(query, page, perPage, newEndpoint) => {
   if (newEndpoint) {
     const response = await axios.get(baseUrl + newEndpoint);
     return response.data;
   }
-  const response = await axios.get(`${baseUrl}/books/search`, {
-    params: { q: query, page, per_page: perPage }
-  });
+  const response = await axios.get(
+      `${baseUrl}/books/search`, {params: {q: query, page, per_page: perPage}});
   return response.data;
 };
 
-export const SearchBookParagraphs = async (
-  bookTitle,
-  start,
-  offset,
-  newEndpoint
-) => {
-  if (newEndpoint) {
-    const response = await axios.get(baseUrl + newEndpoint);
-    return response.data;
-  }
-  const response = await axios.get(`${baseUrl}/books/paragraphs`, {
-    params: { book_title: bookTitle, start, offset }
-  });
-  return response.data;
-};
+export const SearchBookParagraphs =
+    async(bookTitle, start, offset, newEndpoint) => {
+      if (newEndpoint) {
+        const response = await axios.get(baseUrl + newEndpoint);
+        return response.data;
+      }
+      const response =
+          await axios.get(`${baseUrl}/books/paragraphs`,
+                          {params: {book_title: bookTitle, start, offset}});
+      return response.data;
+    };
